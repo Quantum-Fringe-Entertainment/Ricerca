3da367d4393627cdec36e92b31402ad092b6cb16
Pikachuxxxx <phani.s2909@icloud.com>
2020-04-28 20:58:20 +0530

    Modified controls and added gravity

--- WGJ-146/Assets/Scripts/Player/PlayerMovement.cs
+++ WGJ-146/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     public float lookSpeed = 1.5f;
     public float moveSpeed = 5f;
-    public float jumpSpeed = 2.0F;
-    public float gravity = 20.0F;
     private Vector3 moveDirection = Vector3.zero;
 
     [HideInInspector] private CharacterController _charController;
-    [HideInInspector]private Animator _playerAnim;
+    [HideInInspector] private Animator _playerAnim;
     public Camera cam;
 
 
@@ -26,31 +23,28 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        print(moveDirection);
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 lookDir = cam.transform.right * x + cam.transform.forward * z; 
-        if(lookDir != Vector3.zero)
+        moveDirection = cam.transform.right * x + cam.transform.forward * z;
+        if (moveDirection != Vector3.zero)
         {
             //Player Rotation
-            Quaternion rotDir = Quaternion.LookRotation(lookDir);
+            Quaternion rotDir = Quaternion.LookRotation(moveDirection);
             transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x,// X
                 Quaternion.Slerp(transform.rotation, rotDir, Time.deltaTime * lookSpeed).eulerAngles.y, // Y
                 transform.rotation.eulerAngles.z)); // Z
 
             //Player Movement
             moveDirection = transform.forward * moveSpeed * Time.deltaTime;
-            _charController.Move(lookDir * moveSpeed * Time.deltaTime);    
-        }
 
-    
-
-        if(_charController.isGrounded)
-        {
-            print("Yeah Bitch Science!!!");
         }
 
 
+        moveDirection.y += Physics.gravity.y * Time.deltaTime;
+        _charController.Move(moveDirection);
 
 
         if (x > 0 || x < 0 || z > 0 || z < 0)
@@ -62,5 +56,6 @@ public class PlayerMovement : MonoBehaviour
             _playerAnim.SetBool("isWalking", false);
         }
     }//Update
+
 
 }//class PlayerMovement
