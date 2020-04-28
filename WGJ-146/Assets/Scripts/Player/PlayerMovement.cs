d5618c3c67c08892fe35d0d909e853129ad5e8fe
Pikachuxxxx <phani.s2909@icloud.com>
2020-04-28 14:23:29 +0530

    Improved charactercontroller, yet to add jump to it

--- WGJ-146/Assets/Scripts/Player/PlayerMovement.cs
+++ WGJ-146/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,7 +7,11 @@ public class PlayerMovement : MonoBehaviour
 
     public float lookSpeed = 1.5f;
     public float moveSpeed = 5f;
-    [HideInInspector]private CharacterController _charController;
+    public float jumpSpeed = 2.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+
+    [HideInInspector] private CharacterController _charController;
     [HideInInspector]private Animator _playerAnim;
     public Camera cam;
 
@@ -25,24 +29,31 @@ public class PlayerMovement : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 moveDir = cam.transform.right * x + cam.transform.forward * z; 
-        if(moveDir != Vector3.zero)
+        Vector3 lookDir = cam.transform.right * x + cam.transform.forward * z; 
+        if(lookDir != Vector3.zero)
         {
             //Player Rotation
-            Quaternion rotDir = Quaternion.LookRotation(moveDir);
+            Quaternion rotDir = Quaternion.LookRotation(lookDir);
             transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x,// X
                 Quaternion.Slerp(transform.rotation, rotDir, Time.deltaTime * lookSpeed).eulerAngles.y, // Y
                 transform.rotation.eulerAngles.z)); // Z
 
             //Player Movement
-            _charController.Move(transform.forward * moveSpeed * Time.deltaTime);
+            moveDirection = transform.forward * moveSpeed * Time.deltaTime;
+            _charController.Move(lookDir * moveSpeed * Time.deltaTime);    
+        }
 
+    
 
-           
+        if(_charController.isGrounded)
+        {
+            print("Yeah Bitch Science!!!");
         }
 
 
-        if(x > 0 || x < 0 || z > 0 || z < 0)
+
+
+        if (x > 0 || x < 0 || z > 0 || z < 0)
         {
             _playerAnim.SetBool("isWalking", true);
         }
