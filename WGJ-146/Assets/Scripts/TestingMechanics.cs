29450ff3ccf3306de8c675bc0c2d8839b2ee69bb
SaiVivek Amirishetty <vivekboss99@gmail.com>
2020-05-11 16:18:39 +0530

    added script to test easily

---
+++ WGJ-146/Assets/Scripts/TestingMechanics.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingMechanics : MonoBehaviour
+{
+    [Header("Ledge Testing")]
+    public bool LedgeMechanic;
+    public Transform LedgeTest;
+    [Space]
+    [Header("Spike Testing")]
+    public bool SpikeMechanic;
+    public Transform SpikeTest;
+
+    public void Awake()
+    {
+        if (LedgeMechanic == true) {
+            transform.position = LedgeTest.position;
+        }
+        if (SpikeTest == true)
+        {
+            transform.position = SpikeTest.position;
+        }
+       
+    }
+}
