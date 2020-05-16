5f99f092ff54c3c9b511861409bdda8302761abd
Pikachuxxxx <phani.s2909@icloud.com>
2020-05-16 11:14:38 +0530

    Added Broken Ground pieces

--- WGJ-146/Assets/Scripts/Player/PlayerDamage.cs
+++ WGJ-146/Assets/Scripts/Player/PlayerDamage.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
@@ -23,7 +21,6 @@ public class PlayerDamage : MonoBehaviour
     {
         SetRespawnPosition();
     }
-
 
     void SetRespawnPosition()
     {
